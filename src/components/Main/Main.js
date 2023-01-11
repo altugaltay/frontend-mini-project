10686a7b8422f85bd960ab54526ac33f1ba6a06d
altugaltay <altayaltug1997@gmail.com>
2023-01-11 13:28:34 +0300

    refactor: add fetch algorithm main.js style: add some css chore: add axios package

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
 
 function Main() {
+const [loading, setLoading] = useState(true);
+const [data, setData] = useState([])
+useEffect(() => {
+    const fetchData = async () =>{
+      setLoading(true);
+      try {
+        const {data: response} = await axios.get('https://dummyjson.com/products');
+        setData(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
   return (
-    <div className='block col-2'>Main</div>
+    <div className='block col-2'>
+        <h4>Products</h4>
+        {loading && <h4>Loading...</h4>}
+        {!loading && (
+        <div className='row'>
+        {data.products.map(item => (<li key={item.id}>{item.title}</li>))}
+        </div>
+    )}
+    </div>
   )
 }
 
